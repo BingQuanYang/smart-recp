51502d91543eacfe0f2a4f152033dc9266d6c2a9
yangbingquan <yang2606211206@163.com>
2023-03-02 23:43:37 +0800

    [ADD]添加最小价格多少元起

--- smart-recp-service/goods-service/src/main/java/com/smart/recp/service/goods/service/impl/GoodsBuyerServiceImpl.java
+++ smart-recp-service/goods-service/src/main/java/com/smart/recp/service/goods/service/impl/GoodsBuyerServiceImpl.java
@@ -6,13 +6,16 @@ import com.smart.recp.common.core.result.PageResult;
 import com.smart.recp.service.goods.dto.GoodsDTO;
 import com.smart.recp.service.goods.service.GoodsBuyerService;
 import com.smart.recp.service.goods.service.GoodsService;
-import com.smart.recp.service.goods.vo.GoodsCategoryVO;
-import com.smart.recp.service.goods.vo.GoodsVO;
+import com.smart.recp.service.goods.vo.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author ybq
@@ -33,7 +36,27 @@ public class GoodsBuyerServiceImpl implements GoodsBuyerService {
     public PageResult<GoodsVO> list() throws BaseException {
         GoodsDTO goodsDTO = new GoodsDTO();
         goodsDTO.setStatus(1);
+        //TODO
         PageResult<GoodsVO> goodsVOPageResult = goodsService.list(1, 10, goodsDTO);
+        if (ObjectUtils.isNotEmpty(goodsVOPageResult.getList())) {
+            List<Integer> goodsIdList = goodsVOPageResult.getList().stream().map(item -> item.getGoodsId()).collect(Collectors.toList());
+
+            List<GoodsResourceVO> goodsResourceVOList = goodsService.getResourceByGoodsIdListAndIsMaster(goodsIdList, 1);
+
+            Map<Integer, String> map = goodsResourceVOList.stream().collect(Collectors.toMap(GoodsResourceVO::getGoodsId, GoodsResourceVO::getLink, (k, v) -> v));
+
+            goodsVOPageResult.getList().forEach(item -> {
+                item.setImage(map.get(item.getGoodsId()));
+            });
+
+            for (GoodsVO goodsVO : goodsVOPageResult.getList()) {
+                goodsVO.setImage(map.get(goodsVO.getGoodsId()));
+                GoodsVO vo = goodsService.getCascadeByGoodsId(goodsVO.getGoodsId());
+                BigDecimal minPrice = null;
+                minPrice = getMinPrice(vo, minPrice);
+                goodsVO.setMinPrice(minPrice);
+            }
+        }
         return goodsVOPageResult;
     }
 
@@ -56,6 +79,17 @@ public class GoodsBuyerServiceImpl implements GoodsBuyerService {
             log.error("买家端：根据商品ID获取商品信息失败 => goodsId:{}", goodsId);
             throw new BaseException(ResultCode.ERROR);
         }
+    }
+
+    private BigDecimal getMinPrice(GoodsVO goodsVO, BigDecimal minPrice) {
+        for (GoodsSpecVO goodsSpecVO : goodsVO.getGoodsSpecVOList()) {
+            for (GoodsSpecPriceVO goodsSpecPriceVO : goodsSpecVO.getGoodsSpecPriceVOList()) {
+                if (minPrice == null || minPrice.compareTo(goodsSpecPriceVO.getPrice()) == 1) {
+                    minPrice = goodsSpecPriceVO.getPrice();
+                }
+            }
+        }
+        return minPrice;
     }
 
     /**
